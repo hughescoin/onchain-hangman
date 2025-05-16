6f5143219f663426d3650283ed9b376f3c5e49cb
Patrick Hughes <patrick.hughes@coinbase.com>
2025-05-16 16:58:10 -0700

    add minikit functionality

--- app/page.tsx
+++ app/page.tsx
@@ -15,8 +15,19 @@ import {
   EthBalance,
 } from '@coinbase/onchainkit/identity';
 import { PlayButton } from './components/PlayButton';
+import { useMiniKit } from '@coinbase/onchainkit/minikit';
+import { useEffect } from 'react';
 
 export default function App() {
+  const { setFrameReady, isFrameReady } = useMiniKit();
+
+  // The setFrameReady() function is called when your mini-app is ready to be shown
+  useEffect(() => {
+    if (!isFrameReady) {
+      setFrameReady();
+    }
+  }, [setFrameReady, isFrameReady]);
+
   return (
     <div className='flex flex-col min-h-screen font-sans dark:bg-background dark:text-white bg-white text-black'>
       <header className='pt-4 pr-4'>
--- app/providers.tsx
+++ app/providers.tsx
@@ -1,17 +1,17 @@
 'use client';
 
 import { base } from 'wagmi/chains';
-import { OnchainKitProvider } from '@coinbase/onchainkit';
+import { MiniKitProvider } from '@coinbase/onchainkit/minikit';
 import type { ReactNode } from 'react';
 import { PAYMASTER_URL } from './utils/constants';
 export function Providers(props: { children: ReactNode }) {
   return (
-    <OnchainKitProvider
-      apiKey={process.env.NEXT_PUBLIC_ONCHAINKIT_API_KEY}
+    <MiniKitProvider
+      apiKey={process.env.NEXT_PUBLIC_MINIKIT_API_KEY}
       chain={base}
       config={{ appearance: { mode: 'auto' }, paymaster: PAYMASTER_URL }}
     >
       {props.children}
-    </OnchainKitProvider>
+    </MiniKitProvider>
   );
 }
